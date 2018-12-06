5c2902f494a5cb6d57a46ee48c95c5633d06aa3b
charles nseke <nsekecharles@gmail.com>
2018-12-06 01:57:05 +0100

    removing mockk dependency

--- contact-usecases/src/test/kotlin/fr/contact/usecases/user/CreateUserUseCaseTest.kt
+++ contact-usecases/src/test/kotlin/fr/contact/usecases/user/CreateUserUseCaseTest.kt
@@ -3,9 +3,7 @@ package fr.contact.usecases.user
 import fr.contact.core.entities.PhoneNumber
 import fr.contact.core.entities.User
 import fr.contact.usecases.gateways.UserRepository
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import org.amshove.kluent.*
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -13,20 +11,19 @@ import org.jetbrains.spek.api.dsl.on
 
 class CreateUserUseCaseTest : Spek({
 
-    val userRepository = mockk<UserRepository>()
+    val userRepository = mock(UserRepository::class)
     val sut  = CreateUserUseCase(userRepository)
 
     given("A user") {
         val mobilePhoneNumber = PhoneNumber("0812233211")
 
-        every { userRepository.isUserPhoneNumberAvailable(mobilePhoneNumber) } returns true
-
         on("creating the user") {
             val user = User("Charles", mobilePhoneNumber)
-            every { userRepository.save(user) } returns user
+            When calling userRepository.isUserPhoneNumberAvailable(mobilePhoneNumber) itReturns  true
+            When calling userRepository.save(user) itReturns user
             sut.execute(user)
             it("save the user") {
-                verify { userRepository.save(user) }
+                Verify on userRepository that userRepository.save(user) was called
             }
         }
     }
